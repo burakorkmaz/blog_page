0736fc0350bea906b56103ea0a01ca4d88d8f820
burakorkmaz <kkorkmazburakk@gmail.com>
2025-09-11 00:20:20 +0300

    s

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -5,6 +5,9 @@ const uri = process.env.MONGODB_URI as string;
 if (!uri) {
   throw new Error("Please add your Mongo URI to .env.local");
 }
+declare global {
+  var _mongoClientPromise: Promise<MongoClient> | undefined;
+}
 
 let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
