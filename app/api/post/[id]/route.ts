56e40f36fbbac53036156ffee887affba22eb4bc
burakorkmaz <kkorkmazburakk@gmail.com>
2025-09-11 00:23:48 +0300

    sw2

--- app/api/post/[id]/route.ts
+++ app/api/post/[id]/route.ts
@@ -1,15 +1,19 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import clientPromise from "@/lib/mongodb";
 import { ObjectId } from "mongodb";
 
 export async function GET(
-  req: Request,
-  { params }: { params: { id: string } }
+    req: NextRequest,
+    context: { params: { id: string } }
 ) {
   try {
+    const { id } = context.params;
     const client = await clientPromise;
     const db = client.db("blog");
-    const post = await db.collection("posts").findOne({ _id: new ObjectId(params.id) });
+
+    const post = await db
+        .collection("posts")
+        .findOne({ _id: new ObjectId(id) });
 
     if (!post) {
       return NextResponse.json({ error: "Not found" }, { status: 404 });
@@ -18,7 +22,9 @@ export async function GET(
     return NextResponse.json(post);
   } catch (e) {
     console.error(e);
-    return NextResponse.json({ error: "Failed to fetch post" }, { status: 500 });
+    return NextResponse.json(
+        { error: "Failed to fetch post" },
+        { status: 500 }
+    );
   }
 }
-
