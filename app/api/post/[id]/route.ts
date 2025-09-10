2f9777319ab8a637263b0153d5ed2097c2a8405e
burakorkmaz <kkorkmazburakk@gmail.com>
2025-09-11 00:25:22 +0300

    sw4

--- app/api/post/[id]/route.ts
+++ app/api/post/[id]/route.ts
@@ -4,16 +4,15 @@ import { ObjectId } from "mongodb";
 
 export async function GET(
     req: NextRequest,
-    context: { params: { id: string } }
+    { params }: { params: { id: string } }
 ) {
   try {
-    const { id } = context.params;
     const client = await clientPromise;
     const db = client.db("blog");
 
     const post = await db
         .collection("posts")
-        .findOne({ _id: new ObjectId(id) });
+        .findOne({ _id: new ObjectId(params.id) });
 
     if (!post) {
       return NextResponse.json({ error: "Not found" }, { status: 404 });
